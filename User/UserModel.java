c28e9cb13b5ebf1f1ee677614d854d700718e761
mehmoodharis74 <mehmoodharis74@gmail.com>
2022-11-08 11:45:13 +0500

    Chat App

--- User/UserModel.java
+++ User/UserModel.java
@@ -9,59 +9,5 @@ public class UserModel implements Serializable {
     public String userName, lastMessage, lastMessageTime,token, userId;
      public    String userImage, seenUnseenImage;
 
-//        public UserModel() {
-//            this.userName = "John";
-//            this.lastMessage = "Hello";
-//            this.lastMessageTime = "Yesterday";
-//            this.userImage = "";
-//            this.token = null;
-//            //   this.seenUnseenImage = seenUnseenImage;
-//        }
-
-//        public String getUserName() {
-//            return userName;
-//        }
-//
-//        public void setUserName(String userName) {
-//            this.userName = userName;
-//        }
-//
-//        public String getLastMessage() {
-//            return lastMessage;
-//        }
-//
-//        public void setLastMessage(String lastMessage) {
-//            this.lastMessage = lastMessage;
-//        }
-//
-//        public String getLastMessageTime() {
-//            return lastMessageTime;
-//        }
-//
-//        public void setLastMessageTime(String lastMessageTime) {
-//            this.lastMessageTime = lastMessageTime;
-//        }
-//
-//        public void setToken(String token) {
-//            this.token = token;
-//        }
-//        public String getToken() {
-//            return token;
-//        }
-//        public String getUserImage() {
-//            return userImage;
-//        }
-//
-//        public void setUserImage(String userImage) {
-//            this.userImage = userImage;
-//        }
-
-//    public Uri getSeenUnseenImage() {
-//        return seenUnseenImage;
-//    }
-
-//    public void setSeenUnseenImage(Uri seenUnseenImage) {
-//        this.seenUnseenImage = seenUnseenImage;
-//    }
     }
 
